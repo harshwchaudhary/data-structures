18682a67774f7623514cf0175a48244c9b8148f5
Harsh Wardhan Chaudhary <harshwardhan@Harshs-MacBook-Air.local>
2020-04-16 16:24:06 +0530

    update23

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void Insertion(int arr[],int n)
+{
+    int i,j,x;
+    for(i=1;i<n;i++)
+    {
+        j = i-1;
+        x = arr[i];
+        while(j>-1 && arr[j] > x)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = x;
+    }
+}
+
+int main()
+{
+    int A[] = {11,13,7,2,6,9,4,5,10,3};
+    int n = 10,i;
+
+    Insertion(A,n);
+
+    for(i = 0;i<n;i++)
+        cout<<A[i]<<" ";
+    return 0;
+}
